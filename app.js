09953a847285897acf2f1ae61162a534e3d6c608
beniiiiii <56236600+beniiiiii@users.noreply.github.com>
2024-06-08 13:02:20 +0200

    Cors policy

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const  artistRoutes = require("./routes/artist-routes")
 const  songRoutes = require("./routes/song-routes")
 const  playlistRoutes = require("./routes/playlist-routes")
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express()
 
@@ -16,7 +17,9 @@ if(process.env.PORT){
     PORT = process.env.PORT
 }
 
-
+app.use(cors({
+    origin: 'http://localhost:3001' 
+  }));
 app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
