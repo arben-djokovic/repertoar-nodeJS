b5844c9b819bfe28f81c4d92be00eff3d1be83a6
beniiiiii <56236600+beniiiiii@users.noreply.github.com>
2024-09-29 17:56:57 +0200

    test

--- app.js
+++ app.js
@@ -13,12 +13,11 @@ const cors = require('cors');
 const app = express();
 let PORT = process.env.PORT || 3000;
 
-const corsOptions = {
-    origin: 'http://localhost:3000/', // replace with your frontend URL
+
+app.use(cors({
+    origin: '*',
     credentials: true,
-  };
-  
-  app.use(cors(corsOptions));
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
