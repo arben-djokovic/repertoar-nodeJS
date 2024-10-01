28ad2c6c1fa8310ede680af23a1f75553eed1c7b
beniiiiii <56236600+beniiiiii@users.noreply.github.com>
2024-10-01 13:10:54 +0200

    db connection changed

--- data/database.js
+++ data/database.js
@@ -2,12 +2,14 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 let database;
 
+// Use the MongoDB URL from environment variables, fallback to local for development
 const mongodbUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/Repertoar';
 
+// Initialize the MongoClient with options, including Stable API version
 const client = new MongoClient(mongodbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverApi: { version: ServerApiVersion.v1 },
+  serverApi: { version: ServerApiVersion.v1 }, // Ensures backward compatibility with MongoDB 5.0+
 });
 
 const connection = async () => {
@@ -27,9 +29,10 @@ const connection = async () => {
   }
 };
 
-const getDb = () => {
+// Return the database instance if it's connected
+const getDb = async() => {
   if (!database) {
-    throw new Error("Not connected to MongoDB!");
+    await connection()
   }
   return database;
 };
