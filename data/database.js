03f66988583dfa93967888b16a00d25df059733a
beniiiiii <56236600+beniiiiii@users.noreply.github.com>
2024-10-03 01:44:34 +0200

    intermittent connectivity issues fixed

--- data/database.js
+++ data/database.js
@@ -1,33 +1,44 @@
 const { MongoClient } = require('mongodb');
-let database;
 
-let mongodbUrl = ''
-if(process.env.MONGODB_URL){
-    mongodbUrl = process.env.MONGODB_URL
-}
-const connection = async() => {
+let client = null; // Store the MongoDB client
+let database = null; // Store the connected database instance
+
+const connection = async () => {
+  // If the database connection exists, reuse it
+  if (database) {
+    return database;
+  }
+
+  // If client connection is not already created, connect to MongoDB
+  if (!client) {
     try {
-        const client = new MongoClient(process.env.MONGODB_URL);
-        await client.connect();
-        database = client.db('Repertoar');
-    } catch(err) {
-        console.log("Connection error", err);
-        throw err;
+      client = new MongoClient(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      await client.connect();
+      console.log("MongoDB connected successfully");
+    } catch (err) {
+      console.log("MongoDB connection error:", err);
+      throw err;
     }
+  }
+
+  // Set and return the database instance
+  database = client.db('Repertoar');
+  return database;
 };
 
- 
-const getDb = () => {
-    if(database){
-        console.log("connected")
-        return database
-    }
-    console.log("not connected")
-    throw { message: "Not connected"}
-}
+// This function will return the connected database instance
+const getDb = async () => {
+  if (!database) {
+    // Ensure the database is connected before getting the DB instance
+    await connection();
+  }
+  return database;
+};
 
-
-module.exports ={
-    connection,
-    getDb
-}
+module.exports = {
+  connection,
+  getDb,
+};
