c604f638230145d3ef69008dca995048e2721f6e
beniiiiii <56236600+beniiiiii@users.noreply.github.com>
2024-10-02 18:11:24 +0200

    ...

--- data/database.js
+++ data/database.js
@@ -1,13 +1,13 @@
 const { MongoClient } = require('mongodb');
 let database;
-let client;
+
 let mongodbUrl = ''
 if(process.env.MONGODB_URL){
     mongodbUrl = process.env.MONGODB_URL
 }
 const connection = async() => {
     try {
-        client = new MongoClient(process.env.MONGODB_URL);
+        const client = new MongoClient(process.env.MONGODB_URL);
         await client.connect();
         database = client.db('Repertoar');
     } catch(err) {
@@ -17,17 +17,13 @@ const connection = async() => {
 };
 
  
-const getDb = async() => {
+const getDb = () => {
     if(database){
         console.log("connected")
         return database
     }
-    database = await client.db('Repertoar');
-    if(database){
-      return database
-    }else{
-      throw { message: "Not connected"}
-    }
+    console.log("not connected")
+    throw { message: "Not connected"}
 }
 
 
